c29d57df4249f3d4d53474f59c56bae99a018554
mohamadhosseinbahador <mhmdhbhadr@gmail.com>
2024-08-21 15:59:11 +0330

    fix: clear additional codes

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -6,9 +6,8 @@ import { getI18n, getScopedI18n } from "@/locales/server";
 
 export default async function Home() {
   const t = await getI18n();
-  const mmd = "2";
-  console.log("2" === mmd);
   const scopedT = await getScopedI18n("page.hello");
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <Typography variant="h1">{t("page.hello.world")}</Typography>
